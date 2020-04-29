7e8d488d03142cda6375d6addbd1a10753afcc6f
kiranrouthuRS <61677679+kiranrouthuRS@users.noreply.github.com>
2020-04-29 09:09:43 +0530

    backend api url updated
    backend api url updated

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import * as Types from "./definitions";
 
-export const baseApiUrl = "http://3.6.90.1:8000/first_match";
+export const baseApiUrl = "http://3.7.135.210:8000/first_match";
 
 interface PredictionResponse {
   referred_program: string;
